d9bdab625dd68ff1f2b739ac35bfaf7682f6ae77
jipazmino <jpazminomino@cca.edu>
2017-02-05 16:00:09 -0800

    Update sketch-5.js

--- sketch-5.js
+++ sketch-5.js
@@ -8,7 +8,7 @@ function draw() {
 
   var y = 300
 
-    for (var x = 100; x >= 100; x = x + 200) {
+    for (var x = 100; x < width; x = x + 200) {
       for (var x2 = 20; x2 <= 100; x2 = x2 + 20) {
   		rectMode (CENTER);
  			noFill();
